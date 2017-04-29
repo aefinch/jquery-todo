9dbbf34f2b3510545224086611225d230da39e2f
Andrea Finch <aefinch@vt.edu>
2017-04-29 13:45:58 -0500

    todos addable

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('#new-item').click(()=>{
 		$('.list-container').addClass('hide');
 		$('.new-container').removeClass('hide');
+		$('input').focus().val("");
 	});
 	$('#list-items').click(()=>{
 		$('.new-container').addClass('hide');
@@ -14,5 +15,27 @@ $(document).ready(function(){
 	.catch((error)=>{
 		console.log("getTodos Error", error);
 	});
+
+//add todo
+$('#add-todo-button').click(()=>{
+	let newTodo = {
+		isCompleted: false,
+		task: $('#add-todo-text').val()
+	};
+	// console.log(newTodo);
+	FbApi.addTodo(newTodo).then(()=>{
+		$('.new-container').addClass('hide');
+		$('.list-container').removeClass('hide');
+		FbApi.writeDom();
+	}).catch((error)=>{
+		console.log("addTodo error", error);
+	});
+});
+
+//delete todo
+
+//edit todo
+
+//complete todos
 
 });
